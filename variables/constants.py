7a49b4799140858ec6f86a9420a0962e46fedfff
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-02 20:13:42 +0000

    File to store constant variables

---
+++ variables/constants.py
@@ -0,0 +1,26 @@
+# ./variables/constants.py
+
+# CONSTANTS VARIABLES
+
+# Window size
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 1000
+
+# Colours
+COLOUR_BACKGROUND = (0, 0, 0)
+
+# Icon
+ICON_IMG_PATH = './img/target.png'
+
+# Window caption
+WINDOW_TEXT = 'Welcome to Space Shooter'
+
+# Spaceship
+SPACESHIP_IMG_PATH = './assets/assets_img/space_ship.png'
+START_SIZE = (75, 75)
+# Start Speed
+START_SPEED_X = 0
+START_SPEED_Y = 0
+# Start position
+X = (WINDOW_WIDTH / 2) - (75 / 2)
+Y = WINDOW_HEIGHT - 85
