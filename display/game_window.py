b2ff619fc48d719b90725fc626d8e17ddc3e067c
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-03 14:26:40 +0000

    Add spikes list

--- display/game_window.py
+++ display/game_window.py
@@ -4,17 +4,17 @@ from variables import constants as cv
 import pygame
 
 class GameWindow(object):
-
     def __init__(self):
+        # Initialise pygame and clock
         pygame.init()
-
         self.clock = pygame.time.Clock()
 
+        # Create window and window name
         self.surface = pygame.display.set_mode((cv.WINDOW_WIDTH, cv.WINDOW_HEIGHT))
         pygame.display.set_caption(cv.WINDOW_TEXT)
 
+        # Create window icon
         icon_img = pygame.image.load(cv.ICON_IMG_PATH)
         pygame.display.set_icon(icon_img)
 
-        self.space_ship_img = pygame.image.load(cv.SPACESHIP_IMG_PATH)
-        self.space_ship_img = pygame.transform.scale(self.space_ship_img, cv.START_SIZE)
+        self.spikes = []
