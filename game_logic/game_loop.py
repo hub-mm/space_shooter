1db77f0cbd2ef51afc1373264b12010839541b63
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-03 14:27:25 +0000

    Update game_loop to include missiles

--- game_logic/game_loop.py
+++ game_logic/game_loop.py
@@ -1,7 +1,8 @@
 # ./game_logic/game_loop.py
+from variables import constants as cv
 from display.game_window import GameWindow
 from assets.spaceship import SpaceShip
-from variables import constants as cv
+from assets.spaceship_missiles import Missiles
 
 import sys
 import pygame
@@ -10,20 +11,29 @@ class GameLoop(object):
     def __init__(self):
         window = GameWindow()
         spaceship = SpaceShip()
+        missiles = Missiles()
 
         run = True
         while run:
+            # Background
             window.surface.fill(cv.COLOUR_BACKGROUND)
-            window.surface.blit(window.space_ship_img, (cv.X, cv.Y))
+
+            # Spaceship
+            window.surface.blit(spaceship.space_ship_img, (cv.X, cv.Y))
+
             spaceship.move_logic()
 
-            for event in pygame.event.get():
+            # Missile spike
+            missiles.generate_spike()
+            missiles.move_spike()
 
-                if event.type == pygame.QUIT:
+
+            for event in pygame.event.get():
+                keys = pygame.key.get_pressed()
+
+                if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
                     run = False
                     sys.exit()
 
             pygame.display.update()
             window.clock.tick(60)
-
-GameLoop()
