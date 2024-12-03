b305787399da5c7664abba3ececb9aa27d04c15b
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-03 14:24:49 +0000

    Rework logic

--- assets/spaceship.py
+++ assets/spaceship.py
@@ -5,6 +5,10 @@ import pygame
 
 class SpaceShip(object):
     def __init__(self):
+        # Create spaceship image and size
+        self.space_ship_img = pygame.image.load(cv.SPACESHIP_IMG_PATH)
+        self.space_ship_img = pygame.transform.scale(self.space_ship_img, cv.START_SIZE)
+
         self.speed = 5
         self.size = (75, 75)
 
