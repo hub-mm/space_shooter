eeb5db2b4e52fff93be30dd9a696424e21203d48
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-03 14:24:09 +0000

    Update constants

--- variables/constants.py
+++ variables/constants.py
@@ -8,6 +8,7 @@ WINDOW_HEIGHT = 1000
 
 # Colours
 COLOUR_BACKGROUND = (0, 0, 0)
+COLOUR_SPIKE = (255, 0, 0)
 
 # Icon
 ICON_IMG_PATH = './img/target.png'
@@ -18,9 +19,6 @@ WINDOW_TEXT = 'Welcome to Space Shooter'
 # Spaceship
 SPACESHIP_IMG_PATH = './assets/assets_img/space_ship.png'
 START_SIZE = (75, 75)
-# Start Speed
-START_SPEED_X = 0
-START_SPEED_Y = 0
 # Start position
-X = (WINDOW_WIDTH / 2) - (75 / 2)
+X = (WINDOW_WIDTH / 2) - (START_SIZE[0] / 2)
 Y = WINDOW_HEIGHT - 85
