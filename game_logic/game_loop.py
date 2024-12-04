c8b73714fef45b795ad9180764e1445c4031fe0e
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-04 23:44:25 +0000

    Add die ability

--- game_logic/game_loop.py
+++ game_logic/game_loop.py
@@ -1,9 +1,10 @@
 # ./game_logic/game_loop.py
 from variables import constants as cv
 from display.game_window import GameWindow
-from assets.spaceship import SpaceShip
 from assets.spaceship_missiles.bullets import Bullets
 from assets.enemy import Enemy
+from assets.spaceship import SpaceShip
+from display.text_display import TextDisplay
 
 import sys
 import pygame
@@ -11,19 +12,25 @@ import pygame
 class GameLoop(object):
     def __init__(self):
         window = GameWindow()
-        spaceship = SpaceShip()
         bullets = Bullets(window)
         enemy = Enemy(window, bullets)
+        spaceship = SpaceShip(window, enemy)
+        text_display = TextDisplay(window, enemy, spaceship)
 
-
-        run = True
-        while run:
+        self.run = True
+        while self.run:
             # Background
             window.surface.fill(cv.COLOUR_BACKGROUND)
 
+            # Text
+            text_display.display_title()
+            text_display.display_lives()
+            text_display.display_wave()
+
             # Spaceship
-            window.surface.blit(spaceship.space_ship_img, (cv.X, cv.Y))
+            window.surface.blit(spaceship.spaceship_img, (cv.X, cv.Y))
             spaceship.move_logic()
+            spaceship.loose_life()
 
             # Missile bullets
             bullets.generate_missiles()
@@ -33,14 +40,20 @@ class GameLoop(object):
             enemy.generate_enemy()
             enemy.move_enemy()
             enemy.kill_enemy()
-
+            enemy.new_wave()
 
             for event in pygame.event.get():
                 keys = pygame.key.get_pressed()
 
                 if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
-                    run = False
-                    sys.exit()
+                    self.close_game()
+
+            if spaceship.lives <= 0:
+                self.close_game()
 
             pygame.display.update()
             window.clock.tick(60)
+
+    def close_game(self):
+        self.run = False
+        sys.exit()
