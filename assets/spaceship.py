59e45327a9c658f8f6a241e63e9ac1ec65fe380c
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-04 23:44:01 +0000

    Add loose life ability

--- assets/spaceship.py
+++ assets/spaceship.py
@@ -4,13 +4,17 @@ from variables import constants as cv
 import pygame
 
 class SpaceShip(object):
-    def __init__(self):
-        # Create spaceship image and size
-        self.space_ship_img = pygame.image.load(cv.SPACESHIP_IMG_PATH)
-        self.space_ship_img = pygame.transform.scale(self.space_ship_img, cv.START_SIZE)
+    def __init__(self, window, enemy):
+        self.window = window
+        self.enemy = enemy
 
         self.speed = 5
         self.size = (75, 75)
+        self.lives = 5
+
+        # Create spaceship image and size
+        self.spaceship_img = pygame.image.load(cv.SPACESHIP_IMG_PATH)
+        self.spaceship_img = pygame.transform.scale(self.spaceship_img, cv.START_SIZE)
 
     def move_logic(self):
         keys = pygame.key.get_pressed()
@@ -26,3 +30,11 @@ class SpaceShip(object):
 
         if keys[pygame.K_s] and cv.Y <= (cv.WINDOW_HEIGHT - self.size[1]) - 5:
             cv.Y += self.speed
+
+    def loose_life(self):
+        for enemy in self.enemy.window.enemies:
+            if enemy[1] == cv.WINDOW_HEIGHT:
+                self.lives -= 1
+            elif enemy.colliderect(pygame.Rect(cv.X, cv.Y, self.size[0], self.size[1])):
+                self.enemy.window.enemies.remove(enemy)
+                self.lives -= 1
