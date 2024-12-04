f46f2969ac69513402412180bbe9b04fab493be3
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-04 23:41:53 +0000

    Add new wave ability and logic

--- assets/enemy.py
+++ assets/enemy.py
@@ -10,12 +10,18 @@ class Enemy(object):
         self.bullets = bullets
 
         self.speed = 1
-        self.size = (30, 30)
+        self.size = (50, 25)
 
-        self.minimum = 50
-        self.maximum = 100
+        self.minimum = 1
+        self.maximum = 10
         self.enemy_count = 0
+        self.killed = 0
+        self.round = 0
+        self.wave = 1
         self.number_random = random.randint(self.minimum, self.maximum)
+
+        self.enemy_img = pygame.image.load(cv.ALIEN_IMG_PATH)
+        self.enemy_img = pygame.transform.scale(self.enemy_img, self.size)
 
     def generate_enemy(self):
         random_width = random.randint(20, cv.WINDOW_WIDTH - (self.size[0] + 20))
@@ -28,7 +34,7 @@ class Enemy(object):
             self.size[1]
         )
 
-        if self.enemy_count < self.number_random:
+        if self.enemy_count != self.number_random:
             self.enemy_count += 1
             self.window.enemies.append(enemy)
 
@@ -36,13 +42,8 @@ class Enemy(object):
         for i, enemy in enumerate(self.window.enemies):
             enemy[1] += self.speed
 
-            pygame.draw.rect(
-                self.window.surface,
-                'blue',
-                enemy,
-                0,
-                2
-            )
+            x, y = enemy.x, enemy.y
+            self.window.surface.blit(self.enemy_img, (x, y))
 
             rect_one = self.window.enemies[i]
             self.remove_enemy(i, rect_one)
@@ -51,6 +52,8 @@ class Enemy(object):
         for j in range(i + 1, len(self.window.enemies)):
             if rect_one.colliderect(self.window.enemies[j]):
                 self.window.enemies.remove(self.window.enemies[j])
+                self.enemy_count -= 1
+
 
     def kill_enemy(self):
         for enemy in self.window.enemies:
@@ -58,3 +61,25 @@ class Enemy(object):
                 if bullet.colliderect(enemy):
                     self.window.enemies.remove(enemy)
                     self.bullets.missiles.remove(bullet)
+                    self.killed += 1
+
+    def new_wave(self):
+        if self.killed == self.enemy_count or all(enemy[1] > cv.WINDOW_HEIGHT + 100 for enemy in self.window.enemies):
+            self.window.enemies.clear()
+            self.enemy_count = 0
+            self.killed = 0
+            self.round += 1
+            self.new_number()
+
+    def new_number(self):
+        if self.round % 5 == 0:
+            self.wave += 1
+            self.minimum += 5
+            self.maximum += 10
+            self.number_random = random.randint(self.minimum, self.maximum)
+        elif self.round % 5 == 4:
+            self.number_random = self.maximum
+        else:
+            self.number_random = random.randint(self.minimum, self.maximum)
+
+        return self.number_random
