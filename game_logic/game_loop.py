df774c8fbb53b48a4ed1d2f109eb0a44fb6b5155
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-12 21:03:11 +0000

    Update game_loop to reset, track highscore and allow home screen

--- game_logic/game_loop.py
+++ game_logic/game_loop.py
@@ -10,7 +10,7 @@ from assets.enemy import Enemy
 from assets.spaceship import SpaceShip
 from assets.spaceship_powerups.powerups import Powerups
 from game_logic.collision_checks import CollisionChecks
-
+from saved_info.save_info import SaveInfo
 
 
 class GameLoop:
@@ -24,18 +24,31 @@ class GameLoop:
         self.powerup_class = self._create_powerups()
         self.collision_checks = self._create_collision_checks()
         self.text_display = self._create_text_display()
+        self.save_info = self._create_saved_info()
+
+        self.game_state = 'start_menu'
 
         while self.run:
             self._handle_events()
-            self._update_display()
-            self._update_spaceship()
-            self._update_bullets()
-            self._update_enemy()
-            self._update_powerups()
-            self._update_collisions()
 
-            self.enemy.new_wave()
-            self._check_game_over()
+            if self.game_state == 'start_menu':
+                self.window.start_menu()
+                score = self._update_highscore()
+                self._update_start_menu_display(score)
+
+            if self.game_state == 'game':
+                self.window.game()
+
+                self._update_game_display()
+                self._update_spaceship()
+                self._update_bullets()
+                self._update_enemy()
+                self._update_powerups()
+                self._update_collisions()
+
+                self.enemy.new_wave()
+
+                self._check_game_over()
 
             pygame.display.update()
             self.window.clock.tick(60)
@@ -53,6 +66,7 @@ class GameLoop:
         delay_next = 0
         delay = 2000
         maximum = 1
+
         return Bullets(self.window, size, speed, gap_next, gap, delay_next, delay, maximum)
 
     def _create_enemy(self):
@@ -62,10 +76,22 @@ class GameLoop:
         return TextDisplay(self.window, self.enemy, self.spaceship)
 
     def _create_powerups(self):
-        return Powerups(self.window, self.spaceship, self.enemy, self.bullets, 'name', (0, 0), 0, (0, 0))
+        return Powerups(
+            self.window,
+            self.spaceship,
+            self.enemy,
+            self.bullets,
+            'name',
+            (0, 0),
+            0,
+            (0, 0)
+        )
 
     def _create_collision_checks(self):
         return CollisionChecks(self.window, self.bullets, self.enemy, self.spaceship, self.powerup_class)
+
+    def _create_saved_info(self):
+        return SaveInfo(self.enemy, self.spaceship)
 
     def _handle_events(self):
         keys = pygame.key.get_pressed()
@@ -73,9 +99,15 @@ class GameLoop:
             if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
                 self.close_game()
 
-    def _update_display(self):
-        self.window.surface.fill(cv.COLOUR_BACKGROUND)
-        self.text_display.update()
+            if keys[pygame.K_RETURN]:
+                self._reset_game()
+                self.game_state = 'game'
+
+    def _update_start_menu_display(self, score):
+        self.text_display.update_start_menu(score)
+
+    def _update_game_display(self):
+        self.text_display.update_game()
 
     def _update_spaceship(self):
         self.window.surface.blit(self.spaceship.spaceship_img, (cv.X, cv.Y))
@@ -94,9 +126,25 @@ class GameLoop:
     def _update_collisions(self):
         self.collision_checks.update()
 
+    def _update_highscore(self):
+        return self.save_info.update_highscore()
+
     def _check_game_over(self):
         if self.spaceship.lives <= 0:
-            self.close_game()
+            self._update_highscore()
+            self.game_state = 'start_menu'
+
+    def _reset_game(self):
+        self.bullets = self._create_bullets()
+        self.enemy = self._create_enemy()
+        self.spaceship = self._create_spaceship()
+        self.powerup_class = self._create_powerups()
+        self.collision_checks = self._create_collision_checks()
+        self.text_display = self._create_text_display()
+        self.save_info = self._create_saved_info()
+
+        cv.X = cv.START_X
+        cv.Y = cv.START_Y
 
     def close_game(self):
         self.run = False
