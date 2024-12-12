2dde1c3a7ee8c3d6043ee37dcfac94f5ad01335d
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-12 20:59:39 +0000

    Add start screen

--- display/game_window.py
+++ display/game_window.py
@@ -6,14 +6,18 @@ from variables import constants as cv
 
 class GameWindow(object):
     def __init__(self):
-        # Initialise pygame and clock
         pygame.init()
         self.clock = pygame.time.Clock()
 
-        # Create window and window name
         self.surface = pygame.display.set_mode((cv.WINDOW_WIDTH, cv.WINDOW_HEIGHT))
-        pygame.display.set_caption(cv.WINDOW_TEXT)
 
-        # Create window icon
         icon_img = pygame.image.load(cv.ICON_IMG_PATH)
         pygame.display.set_icon(icon_img)
+
+    def start_menu(self):
+        self.surface.fill(cv.COLOUR_BACKGROUND)
+        pygame.display.set_caption(cv.WINDOW_START_TEXT)
+
+    def game(self):
+        self.surface.fill(cv.COLOUR_BACKGROUND)
+        pygame.display.set_caption(cv.WINDOW_TEXT)
