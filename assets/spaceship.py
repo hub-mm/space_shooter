4e764c3e693fdda93d7634405dcf852125e4b174
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-12 21:01:33 +0000

    Remove unnecessary comments

--- assets/spaceship.py
+++ assets/spaceship.py
@@ -13,7 +13,6 @@ class SpaceShip(object):
         self.lives = 5
         self.coins = 0
 
-        # Create spaceship image and size
         self.spaceship_img = pygame.image.load(cv.SPACESHIP_IMG_PATH)
         self.spaceship_img = pygame.transform.scale(self.spaceship_img, cv.START_SIZE)
 
