7393de24457029e2bb2915d3196fcd93f75ddf4a
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-12 16:08:20 +0000

    Rework to work with sprite group, taking collision logic out and add controls

--- assets/spaceship.py
+++ assets/spaceship.py
@@ -1,40 +1,38 @@
 # ./assets/spaceship.py
+import pygame
+
 from variables import constants as cv
 
-import pygame
 
 class SpaceShip(object):
     def __init__(self, window, enemy, speed, size):
         self.window = window
         self.enemy = enemy
-
-        self.speed = 5
-        self.size = (75, 75)
+        self.speed = speed
+        self.size = size
         self.lives = 5
+        self.coins = 0
 
         # Create spaceship image and size
         self.spaceship_img = pygame.image.load(cv.SPACESHIP_IMG_PATH)
         self.spaceship_img = pygame.transform.scale(self.spaceship_img, cv.START_SIZE)
 
-    def move_logic(self):
+    def update(self):
+        self._handle_movement()
+
+    def _handle_movement(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a] and cv.X >= 5:
+        if keys[pygame.K_a] and cv.X >= 5 or keys[pygame.K_LEFT] and cv.X >= 5:
             cv.X -= self.speed
-
-        if keys[pygame.K_d] and cv.X <= (cv.WINDOW_WIDTH - self.size[0]) - 5:
+        if (keys[pygame.K_d] and cv.X <= (cv.WINDOW_WIDTH - self.size[0]) - 5
+                or keys[pygame.K_RIGHT] and cv.X <= (cv.WINDOW_WIDTH - self.size[0]) - 5):
             cv.X += self.speed
-
-        if keys[pygame.K_w] and cv.Y >= 5:
+        if keys[pygame.K_w] and cv.Y >= 5 or keys[pygame.K_UP] and cv.Y >= 5:
             cv.Y -= self.speed
-
-        if keys[pygame.K_s] and cv.Y <= (cv.WINDOW_HEIGHT - self.size[1]) - 5:
+        if (keys[pygame.K_s] and cv.Y <= (cv.WINDOW_HEIGHT - self.size[1]) - 5
+                or keys[pygame.K_DOWN] and cv.Y <= (cv.WINDOW_HEIGHT - self.size[1]) - 5):
             cv.Y += self.speed
 
-    def loose_life(self):
-        for enemy in self.enemy.window.enemies:
-            if enemy[1] == cv.WINDOW_HEIGHT:
-                self.lives -= 1
-            elif enemy.colliderect(pygame.Rect(cv.X, cv.Y, self.size[0], self.size[1])):
-                self.enemy.window.enemies.remove(enemy)
-                self.lives -= 1
+    def lose_life(self):
+        self.lives -= 1
