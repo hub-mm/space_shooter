6841f4742dc47e9bcba47bdc9d750514cebc27ba
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-14 00:29:21 +0000

    Add ability to track total coins and remove when item brought

--- saved_info/coins/info_coins.py
+++ saved_info/coins/info_coins.py
@@ -1,39 +1,42 @@
 # ./saved_info/coins/info_coins.py
+import os
 
 
 class InfoCoins(object):
     def __init__(self, spaceship):
+        self.file_path = './saved_info/coins/coins.txt'
         self.spaceship = spaceship
-        self.total_coins = self._read_coins()
         self._ensure_file_exists()
+        self.total_coins = self._read_coins()
 
     def _ensure_file_exists(self):
-        try:
-            with open('./saved_info/coins/coins.txt', 'r') as read_coins:
-                content = read_coins.read().strip()
-                self.total_coins = int(content) if content.isdigit() else 0
-        except FileNotFoundError:
-            with open('./saved_info/coins/coins.txt', 'w') as write_coins:
-                write_coins.write('0')
-
-            self.total_coins = 0
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, 'w') as f:
+                f.write('0')
 
     def update_coins(self):
-        current_coins = self.spaceship.coins
-        self.total_coins += current_coins
+        self._write_coins()
+        return self.total_coins
+
+    def set_total_coins(self, new_total):
+        self.total_coins = new_total
+        self._write_coins()
+        return self.total_coins
+
+    def add_coins(self, amount):
+        self.total_coins += amount
         self._write_coins()
         return self.total_coins
 
     def _write_coins(self):
-        with open('./saved_info/coins/coins.txt', 'w') as write_coins:
+        with open(self.file_path, 'w') as write_coins:
             write_coins.write(str(self.total_coins))
 
     def _read_coins(self):
         try:
-            with open('./saved_info/coins/coins.txt', 'r') as read_coins:
+            with open(self.file_path, 'r') as read_coins:
                 content = read_coins.read().strip()
-                self.total_coins = int(content) if content.isdigit() else 0
+                coins = int(content) if content.isdigit() else 0
+                return coins
         except FileNotFoundError:
-            self.total_coins = 0
-
-        return self.total_coins
+            return 0
